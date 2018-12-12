0f26d9c468c0c5279328c650c13b4cc74b6e6754
renxiaochuan <15002742523@163.com>
2018-12-12 10:54:38 +0800

    测试代码

---
+++ security/src/main/java/com/example/security/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DemoController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+}
